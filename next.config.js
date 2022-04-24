fc665770eeb4104af28a125fd03ab79b37777be0
Maestroharyor <haryor1josh@gmail.com>
2022-04-24 03:42:10 +0100

    Removed styled-components

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
   env: {
     title: "Lifetechfacts Get Into Tech Program",
     titleDescription: "The best tech/coding mentorship program for you"
