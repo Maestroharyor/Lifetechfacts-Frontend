a5eec3737e3031243e84cac9b0089546c90ca1d6
Maestroharyor <haryor1josh@gmail.com>
2022-04-28 01:54:49 +0100

    Added google tag code

--- components/Layouts/MasterLayout.tsx
+++ components/Layouts/MasterLayout.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { connect, useDispatch } from "react-redux";
 import { setDarkModeTheme, setLightModeTheme } from "../../store/theme/action";
 import { themeChange, themeOSLoad } from "../../functions/theme";
@@ -7,7 +8,7 @@ import { themeChange, themeOSLoad } from "../../functions/theme";
 
 type Props = {};
 
-const MasterLayout = (props:any) => {
+const MasterLayout = (props: any) => {
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -22,14 +23,37 @@ const MasterLayout = (props:any) => {
     <>
       {props.children}
       {/* <Backtotop /> */}
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4J366QMVGZ"
+      ></Script>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-4J366QMVGZ');
+  `,
+        }}
+      />
+
+      {/* <Script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4J366QMVGZ');
+</Script> */}
     </>
   );
 };
 
-const mapStateToProps = (state:any) => {
-    return state;
-  };
-
-
+const mapStateToProps = (state: any) => {
+  return state;
+};
 
 export default connect(mapStateToProps)(MasterLayout);
